7c241aa39a825cf23ae58e565cf55cf95bc1062f
darkhawk0000 <liur3@tcnj.edu>
2019-10-28 09:19:24 -0400

    Added convolution and frequency response of convolution

--- Lab4B/venv/src/main.py
+++ Lab4B/venv/src/main.py
@@ -25,10 +25,9 @@ x_fft = fftpack.fft(x_t)
 freqs = fftpack.fftfreq(len(x_t)) * f_s
 
 plt.figure()
-#plt.plot(t, x_t)
-#plt.stem(freqs, np.abs(x_fft), use_line_collection=True)
+#plt.plot(t, x_t) #part 1
+#plt.stem(freqs, np.abs(x_fft), use_line_collection=True) #part 2
 #plt.xlim(0, 4300)
-
 
 
 
@@ -40,15 +39,21 @@ taps = signal.remez(numtaps, [0, cutoff, cutoff + trans_width, 0.5*fs], [1, 0], 
 w, h = signal.freqz(taps, [1], worN=1024)
 remez_filter = 0.5*fs*w/np.pi
 remez_y = 20*np.log10(np.abs(h))
-#plt.plot(remez_filter, remez_y)
+#plt.plot(remez_filter, remez_y) #part 3
 
 filtered = signal.convolve(x_t, remez_filter, mode='same')
-filter_fft = fftpack.fft(filtered)
-filter_freqs = fftpack.fftfreq(len(filtered)) * fs
-plt.stem(filter_freqs, np.abs(filter_fft), use_line_collection=True)
-plt.xlim(0, 4300)
+#plt.plot(filtered) #part 4
 
-#plt.plot(filtered, remez_y)
+filter_fft = fftpack.fft(remez_filter)
+filter_freqs = fftpack.fftfreq(len(remez_filter)) * fs
+#plt.stem(filter_freqs, np.abs(filter_fft), use_line_collection=True)
+#plt.xlim(-150, 4300) #part 5
+
+convolve_fft = fftpack.fft(filtered)
+convolve_freqs = fftpack.fftfreq(len(filtered)) * fs
+plt.stem(convolve_freqs, np.abs(convolve_fft), use_line_collection=True)
+plt.xlim(-150, 4300) #part 6
+
 
 
 plt.show()
